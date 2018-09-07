e3f30fe2181cdb2f5593383ef1b056147eb96a1f
陈红名 <31301644+AIMarkov@users.noreply.github.com>
2018-09-07 11:40:16 +0800

    Create baseline.py

---
+++ original/config/baseline.py
@@ -0,0 +1,18 @@
+# rl parameters
+max_episodes = 80000
+max_steps = 1000
+max_buffer = 1000000
+replay_start_size = 50
+
+# noise parameters
+mu = 0
+theta = 0.15
+sigma = 0.2
+dt = 1e-2
+
+actor_learning_rate = 1e-4
+critic_learning_rate = 1e-3
+batch_size = 64
+
+gamma = 0.99
+tau = 0.001
